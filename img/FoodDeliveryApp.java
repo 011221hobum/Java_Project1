3c3c16ba57ac3b9a34ea1cd720da19ac877a2ba5
gwaghobeom <gwaghobeom@172.16.10.23>
2024-10-22 11:30:49 +0900

    test

---
+++ img/FoodDeliveryApp.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodDeliveryApp {
+
+    private static FoodDeliveryApp instance;
+    private List<Category> categories;
+
+    private FoodDeliveryApp(){
+        categories = new ArrayList<>();
+
+    }
+
+    static synchronized FoodDeliveryApp getInstance(){
+        if(instance == null){
+            instance = new FoodDeliveryApp();
+        }
+        return instance;
+    }
+    void addCategory(Category category){
+        categories.add(category);
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+}
