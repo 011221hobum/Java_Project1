7b5930254b057c51ce2e11bab592015a7db0a759
011221hobum <uyop011221@gmail.com>
2024-10-22 10:51:28 +0900

    project_ver01
    20241022_1차 초안

---
+++ src/firstpro/main/Main.java
@@ -0,0 +1,14 @@
+package firstpro.main;
+
+import frame.FrameBase;
+import frame.FrameHome;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FrameBase.getInstance(new FrameHome(null));
+		
+		
+	}
+
+}
---
+++ src/restaurant/Frame1_1.java
@@ -0,0 +1,7 @@
+package restaurant;
+
+import javax.swing.JPanel;
+
+public class Frame1_1 extends JPanel{
+	
+}
